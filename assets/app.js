56963ee79fc947de4d53daefd7214b3ec3f787e7
ar0ne <camtbitakou@yandex.ru>
2015-02-08 18:18:27 +0300

    add menu

--- assets/app.js
+++ assets/app.js
@@ -9,26 +9,44 @@
         var canvas = document.getElementById('canvas');
         var screen = canvas.getContext('2d');
 
+        var self = this;
+
         this.gameSize = {
             width:  canvas.width,
             height: canvas.height
         };
 
-        var self = this;
-
         this.block_height = this.gameSize.height / 15;   // 32px
+
+        this.lives = 0;
 
         this.bodies = [];
 
-        this.bodies = this.bodies.concat(load_level( this, LEVELS.level_1 ));
-
-        var tick = function () {
+        var tick = function () {   
             self.update();
             self.draw(screen, self.gameSize);
             requestAnimationFrame(tick);
         };
 
-        tick();
+        this.menu(screen);
+
+        window.addEventListener("mousedown", doMouseDown, false);
+
+        function doMouseDown(event) {
+
+            var x = event.pageX - canvas.offsetLeft,
+                y = event.pageY - canvas.offsetTop;
+
+                if (x >= self.gameSize.width / 2 - 95  && x <= self.gameSize.width / 2 + 100 &&
+                    y >= self.gameSize.height / 2 - 45 && y <= self.gameSize.height / 2 + 40) {
+
+                    self.bodies = self.bodies.concat(load_level( self, LEVELS.level_1 ));
+
+                    tick();
+                }
+
+            window.removeEventListener("mousedown", doMouseDown, false);
+        }
 
     };
 
@@ -49,11 +67,11 @@
 
                             if ( this.isCollided(this.bodies[i], this.bodies[j])) {
 
-                                if ( this.bodies[i].speedX !== 0 ) {
+                                if ( this.bodies[i].speedX !== 0 || this.bodies[i].speedY !== 0) {
 
                                     this.bodies.splice(j, 1);
                                     this.bodies.splice(i, 1);
-
+                                  
                                 } else {
 
                                     this.bodies[j].position = JSON.parse(JSON.stringify(this.bodies[j].old_position));
@@ -65,7 +83,7 @@
 
                             if ( this.isCollided(this.bodies[i], this.bodies[j])) {
 
-                                if( this.bodies[j].speedX !== 0 ) {
+                                if( this.bodies[j].speedX !== 0 || this.bodies[j].speedY !== 0) {
 
                                     this.bodies.splice(j, 1);
                                     this.bodies.splice(i, 1);
@@ -103,6 +121,28 @@
                 b1.position.y  >= b2.position.y + b2.size.height
             );
         },
+        menu: function (screen) {           
+           
+            screen.font = '50px Monospace';
+            // background
+            screen.fillStyle = '#56E8BC';
+            screen.fillRect(0, 0, this.gameSize.width, this.gameSize.height);
+
+            // add button start
+            screen.fillStyle = "#C43963";
+            screen.fillRect(this.gameSize.width / 2 - 95, this.gameSize.height / 2 - 45, 200, 100);
+            screen.fillStyle = "#E85682";
+            screen.fillRect(this.gameSize.width / 2 - 100, this.gameSize.height / 2 - 50, 200, 100);
+
+            screen.fillStyle = 'white';
+            screen.fillText('Start', this.gameSize.width / 2 - 70, this.gameSize.height / 2 + 20);
+
+            screen.fillText('The Frog', this.gameSize.width / 2 - 120, 100);
+
+            screen.font = '15px Monospace';
+            screen.fillText('https://github.com/ar0ne/frog_html5_game', 150, this.gameSize.height - 20);  
+
+        }
 
     };
 
@@ -177,6 +217,7 @@
 
 
     var Wall = function (options) {
+
         this.game = options.game;
         this.gameSize = this.game.gameSize;
         this.size =  {
@@ -186,6 +227,8 @@
         this.position = options.position;
 
         this.speedX = options.speedX;
+        this.speedY = options.speedY;
+
 
         this.timer = 0;
 
@@ -194,20 +237,41 @@
 
     Wall.prototype = {
         update: function () {
-            if(this.speedX == 0) {
+            if(this.speedX === 0 && this.speedY === 0) {
+                // it's something static
                 return;
             }
+
             if (this.speedX > 0 && this.position.x > this.game.gameSize.width) {
-                this.position.x = - this.size.width;
+                this.position.x = 0 - this.size.width;
             } else if (this.speedX < 0 && this.position.x < - this.size.width) {
                 this.position.x = this.game.gameSize.width;
             }
 
+            if (this.speedY > 0 && this.position.y < 0) {
+                this.position.y = this.game.gameSize.height;
+            } else if (this.speedY < 0 && this.position.y > this.gameSize.height) {
+                this.position.y = - this.size.height;
+            }
+
+
             this.timer++;
 
             if(this.timer % 23 == 0) {
+
                 this.timer = 0;
-                this.position.x +=  this.speedX > 0 ? this.game.block_height : - this.game.block_height ;
+
+                if(this.speedX > 0) {
+                    this.position.x +=   this.game.block_height;
+                } else if (this.speedX < 0) {
+                    this.position.x += - this.game.block_height;
+                }
+
+                if(this.speedY > 0) {
+                    this.position.y += - this.game.block_height;
+                } else if (this.speedY < 0) {
+                    this.position.y +=   this.game.block_height;
+                }
             }
 
         },
@@ -249,19 +313,28 @@
 
         var rows        = level.map.length,
             columns     = level.map[0].length,
-            bodies      = [];
+            bodies      = [],
+            KEYS        = {
+                EMPTY:          0,
+                WALL_STATIC:    1,
+                WALL_MOVE_X:    2,
+                WALL_MOVE_Y:    3,
+                PLAYER:         4,
+                EXIT:           5
+            };
 
         for( var i = 0; i < rows; i++) {
-            var speedX = level.speedX[i];
+            var speedX = level.speedX[i],
+                speedY = level.speedY[i];
 
             for( var j = 0; j < columns; j++) {
 
                 // empty fields
-                if (level.map[i][j] === 0) {
+                if (level.map[i][j] === KEYS.EMPTY) {
                     continue;
                 }
 
-                if ( level.map[i][j] === 1 || level.map[i][j] === 2) {
+                if ( level.map[i][j] === KEYS.WALL_STATIC || level.map[i][j] === KEYS.WALL_MOVE_X || level.map[i][j] === KEYS.WALL_MOVE_Y) {
                     bodies.push( new Wall ({
                         game: game,
                         position: {
@@ -269,10 +342,12 @@
                             y: game.block_height * i
                         },
                         // if field is dynamic than it have speed
-                        speedX: (level.map[i][j] === 2 ? speedX : 0)
+                        speedX: (level.map[i][j] === KEYS.WALL_MOVE_X ? speedX : 0),
+                        speedY: (level.map[i][j] === KEYS.WALL_MOVE_Y ? speedY : 0),
+
                     }));
 
-                } else if( level.map[i][j] === 3 ){
+                } else if(level.map[i][j] === KEYS.PLAYER){
 
                     bodies.push( new Player ({
                         game: game,
@@ -297,9 +372,10 @@
      *  map size 15 x 20
      *  0 - empty field
      *  1 - wall static
-     *  2 - wall dynamic
-     *  3 - player
-     *  4 - exit
+     *  2 - wall dynamic by axis X
+     *  3 - wall dynamic by axis Y
+     *  4 - player
+     *  5 - exit
     **/
     var LEVELS =  {
 
@@ -313,16 +389,19 @@
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 6
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 7
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 8
-                    [1, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 9
+                    [1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 9
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 10
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 11
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 12
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 13
                     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],   // 14
-                    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 1],   // 15
+                    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 1],   // 15
                    ],
-               "speedX": [ 0, 0, 0, 0, 0, 0, 0, 0, 2,  0,  0,  0,  0,  0,  0]
-                //         1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+                "speedX": [ 0, 0, 0, 0, 0, 0, 0, 0, 2,  0,  0,  0,  0,  0,  0],
+                //          1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+
+                "speedY": [ 0, 0, 0, 0, 0, 0, 0, 0, -2,  0,  0,  0,  0,  0,  0],
+                //          1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
        },
 
        "level_2":  {
